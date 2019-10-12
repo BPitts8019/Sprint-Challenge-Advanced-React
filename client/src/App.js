de355e2c3d084bfe577e95214dc3ee66e9af7669
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-11 17:00:46 -0700

    converted App to class component; added scaffolding for other components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-function App() {
-   return (
-      <div className="App">
-         <header className="App-header">
-            <h1>This is an App!</h1>
-         </header>
-      </div>
-   );
+class App extends React.Component {
+   // constructor () {
+
+   // }
+
+   render () {
+      return (
+         <div className="App">
+            <header className="App-header">
+               <h1>This is an App!</h1>
+            </header>
+         </div>
+      );
+   }
 }
+
+
+// function App() {
+//    return (
+//       <div className="App">
+//          <header className="App-header">
+//             <h1>This is an App!</h1>
+//          </header>
+//       </div>
+//    );
+// }
 
 export default App;
