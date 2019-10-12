4e4694972060a94692a7a0fe53accd4ffb6b5748
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-11 17:25:19 -0700

    Added testing for PlayerList

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-function Player () {
+function Player ({name, country, searches}) {
    return (
-      <div>
-         
-      </div>
+      <div data-testid="player-card"></div>
    );
 }
 
--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Player from "./Player.js";
 
-function PlayerList () {
+function PlayerList ({players}) {
    return (
-      <div>
-         
+      <div data-testid="player-list" className="player-list">
+         {players.map((player, index) => <Player key={index} {...player} />)}
       </div>
    );
 }
