ebf9cb9656c53dfa8a2386d0b6f082125c4a1761
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-11 18:21:43 -0700

    Player data displayed on screen

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-function Player ({name, country, searches}) {
+function Player({ name, country, searches }) {
    return (
-      <div data-testid="player-card"></div>
+      <div data-testid="player-card" className="card">
+         <h3 className="name">{name}</h3>
+         <p>{`Location: ${country}`}</p>
+         <p>{`Searches: ${searches}`}</p>
+      </div>
    );
 }
 
